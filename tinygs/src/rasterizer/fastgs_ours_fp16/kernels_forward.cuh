8f466ee4b9515bc29899b35587ad338e9fba5d9c
Adversarr <1444781838@qq.com>
2025-10-26 14:55:16 +0800

    Fix low version cuda build error

--- tinygs/src/rasterizer/fastgs_ours_fp16/kernels_forward.cuh
+++ tinygs/src/rasterizer/fastgs_ours_fp16/kernels_forward.cuh
@@ -1158,7 +1158,7 @@ __global__ void __launch_bounds__(config::block_size_blend / 2) blend_cu2(
     using BlockReduce = cub::BlockReduce<ushort, config::tile_width / 2, cub::BLOCK_REDUCE_WARP_REDUCTIONS, config::tile_width>;
     __shared__ typename BlockReduce::TempStorage temp_storage;
     ushort max_xy = n_contributions.data.x > n_contributions.data.y ? n_contributions.data.x : n_contributions.data.y;
-    max_xy = BlockReduce(temp_storage).Reduce(max_xy, cuda::maximum<ushort>());
+    max_xy = BlockReduce(temp_storage).Reduce(max_xy, cub::Max());
 
     if (thread_rank == 0) {
 #ifndef NDEBUG
